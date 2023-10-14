7f989232ca61f6148d2694415cecc5cfef6d61be
salviega <salviega6@gmail.com>
2023-10-14 07:53:33 -0500

    :tada: Listener backend was started

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cross Carbon - listener
+
+Cross Carbon: interoperability carbon offset dApp
+
+## Quick start 🏄
+
+Prerequisites: [Node (v19 LTS)](https://nodejs.org/en/download/) and [Git](https://git-scm.com/downloads)
+
+> Clone the repository:
+
+```
+git clone https://github.com/salviega/cross-carbon-backend-listener.git
+```
+
+> Install dependencies:
+
+```
+yarn
+```
+
+> Run the project:
+
+```
+yarn start
+```
+
+## Authors 🏗
+
+[salviega](https://github.com/salviega)
---
+++ index.js
@@ -0,0 +1,73 @@
+import ethers from 'ethers'
+import dotenv from 'dotenv'
+import calculatorContractJson from './assets/json/contracts/Calculator.json'
+import carbonContractJson from './assets/json/contracts/Carbon.json'
+dotenv.config()
+
+const { POLYGON_MUMBAI_RPC_URL, PRIVATE_KEY } = process.env
+
+if (!POLYGON_MUMBAI_RPC_URL || !PRIVATE_KEY) {
+	throw new Error(
+		'POLYGON_MUMBAI_RPC_URL or PRIVATE_KEY not found in .env file'
+	)
+}
+
+const scan = 'https://mumbai.polygonscan.com/tx'
+
+const provider = new ethers.providers.JsonRpcProvider(POLYGON_MUMBAI_RPC_URL)
+
+const wallet = new ethers.Wallet(PRIVATE_KEY, provider)
+const signer = wallet.connect(provider)
+
+const calculatorContract = new ethers.Contract(
+	calculatorContractJson.address,
+	calculatorContractJson.abi,
+	provider
+)
+
+const carbonContract = new ethers.Contract(
+	carbonContractJson.address,
+	carbonContractJson.abi,
+	signer
+)
+
+const event = 'CarbonFootprintOffset'
+
+const callback = async (requestId, flag, args, values, buyer) => {
+	console.log('requestId: ', requestId)
+	console.log('flag: ', flag)
+	console.log('args: ', args)
+	console.log('values: ', values)
+	console.log('buyer: ', buyer)
+
+	console.log('\n')
+
+	const object = {
+		requestId,
+		flag,
+		args,
+		values,
+		buyer
+	}
+
+	console.debug(object)
+
+	try {
+		const offsetFootprintTx = await carbonContract.offsetFootprint(
+			requestId,
+			flag,
+			args,
+			values,
+			buyer
+		)
+		offsetFootprintTx.wait(1)
+
+		console.log('\n')
+		console.log('Transaction was successful 🎉')
+		console.log(`see transaction on ${scan}/${offsetFootprintTx.hash}`)
+	} catch (error) {
+		console.error('error: ', error)
+	}
+}
+
+calculatorContract.on(event, callback)
